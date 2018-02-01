9c1a86f80c2dd08c7768c0d36c2e6f3d3a59912e
Dawid Worhacz <worhacz.dawid@gmail.com>
2018-02-01 13:29:15 +0100

    zarzadzanie slajderem partnerzy z poziomu admina

--- merkuriusz/template/slider-partnerzy.php
+++ merkuriusz/template/slider-partnerzy.php
@@ -8,72 +8,26 @@
 		</h2>
 		<div class="col-md-12 partner-container partner-slider" style="padding: 0;">
 			<div class="partner-arrow-box partner-left">
-			<i class="fa fa-angle-left fa-2x arrow-position" aria-hidden="true"></i>
+				<i class="fa fa-angle-left fa-2x arrow-position" aria-hidden="true"></i>
 			</div>
-
 
 			<div class="partner-wrapper">
+				<?php
+					$posts = get_posts( array(
+						'numberposts' => -1,
+						'category_name' => 'Nasi partnerzy',
+						'orderby' => 'rand',
+						
+					) );
+					
+					foreach( $posts as $item):
+				?>
 				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/bc.svg" class="partner-icon">
+					<img src="<?php echo get_the_post_thumbnail_url( $item->ID, 'medium' ); ?>" class="partner-icon">
 				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/sols.svg" class="partner-icon">
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/sg.svg" class="partner-icon">
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/fruit.png" class="partner-icon"></div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/logo_adler.svg" class="partner-icon">
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/alex_fox.svg" class="partner-icon"></div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/jhk.svg" class="partner-icon" >
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/macma.svg" class="partner-icon" >
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/happy_gifts.png" class="partner-icon">
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/waterman.svg" class="partner-icon" >
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/Parker-logo-new-and-original.png" class="partner-icon">
-				</div>
-
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/nina_rici.svg" class="partner-icon" >
-				</div>
-				
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/diplomat.svg" class="partner-icon" >
-				</div>
-			
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/blue_collection.svg" class="partner-icon" >
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/royal_design.svg" class="partner-icon">
-				</div>
-				
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/cool.svg" class="partner-icon">
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/easy_gifts.svg" class="partner-icon">
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/tops.svg" class="partner-icon">
-				</div>
-				<div class="partner-icon-box">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/partnerzy/voyager-XD.png" class="partner-icon"></div>
+				<?php endforeach; ?>
 			</div>
-
-
+				
 			<div class="partner-arrow-box partner-right"><i class="fa fa-angle-right fa-2x arrow-position" aria-hidden="true"></i></div>
 
 		</div>
